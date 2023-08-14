84276c0783ee3380f25d73131d342b3fc9109e97
4nth0ny1 <acatullo4@gmail.com>
2023-08-13 22:40:08 -0500

    editTopic

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,9 +1,30 @@
 import EditTopicForm from "@/components/EditTopicForm";
 
-export default function EditTopic() {
+const getTopicById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function EditTopic({ params }) {
+  const { id } = params; // because dynamic route we get the id from params
+
+  const { topic } = await getTopicById(id);
+  const { title, description } = topic;
+
   return (
     <div>
-      <EditTopicForm />
+      <EditTopicForm title={title} description={description} id={id} />
     </div>
   );
 }
--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,16 +1,56 @@
-export default function EditTopicForm() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function EditTopicForm({ id, title, description }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and description are required.");
+      return;
+    }
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+
+      if (res.ok) {
+        router.refresh();
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="flex flex-col gap-2">
         <input
           type="text"
-          placeholder="Topic Title"
-          classname="border-slate-500 border "
+          value={newTitle}
+          className="border-slate-500 border "
+          onChange={(e) => setNewTitle(e.target.value)}
         />
         <input
           type="text"
-          placeholder="Topic Description"
-          classname="border-slate-500 border-2 w-full"
+          value={newDescription}
+          className="border-slate-500 border-2 w-full"
+          onChange={(e) => setNewDescription(e.target.value)}
         />
       </div>
       <button className="flex flex-end px-2 border-slate-500 border rounded-xl my-2 w-fit">
