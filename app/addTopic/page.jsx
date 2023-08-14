3e0b9b73087e8cd2af016d2b45f6d111d1ff5092
4nth0ny1 <acatullo4@gmail.com>
2023-08-13 22:17:21 -0500

    addTopic

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and description are required.");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="flex flex-col gap-2">
         <input
           type="text"
           placeholder="Topic Title"
-          classname="border-slate-500 border "
+          className="border-slate-500 border "
+          onChange={(e) => setTitle(e.target.value)}
         />
         <input
           type="text"
           placeholder="Topic Description"
-          classname="border-slate-500 border-2 w-full"
+          className="border-slate-500 border-2 w-full"
+          onChange={(e) => setDescription(e.target.value)}
         />
       </div>
       <button className="flex flex-end px-2 border-slate-500 border rounded-xl my-2 w-fit">
