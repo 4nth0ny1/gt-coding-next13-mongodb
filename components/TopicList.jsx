f8cd115b0b20977ca99db0500493fa71622ff6f5
4nth0ny1 <acatullo4@gmail.com>
2023-08-13 20:20:53 -0500

    rending data in client

--- components/TopicList.jsx
+++ components/TopicList.jsx
@@ -2,19 +2,41 @@ import React from "react";
 import RemoveButton from "./RemoveButton";
 import Link from "next/link";
 
-const TopicList = () => {
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+const TopicList = async () => {
+  const { topics } = await getTopics();
+
   return (
     <>
-      <div className="flex justify-between border-slate-300 border-2 p-4 rounded-xl bg-slate-100 shadow-lg mb-4">
-        <div>
-          <h2 className="text-2xl">Topic Title</h2>
-          <div>Topic Description</div>
-        </div>
-        <div className="flex gap-2">
-          <RemoveButton />
-          <Link href="/editTopic/123">edit</Link>
-        </div>
-      </div>
+      {topics.map((topic) => {
+        return (
+          <div className="flex justify-between border-slate-300 border-2 p-4 rounded-xl bg-slate-100 shadow-lg mb-4">
+            <div>
+              <h2 className="text-2xl">{topic.title}</h2>
+              <div>{topic.description}</div>
+            </div>
+            <div className="flex gap-2">
+              <RemoveButton />
+              <Link href={`/editTopic/${topic.id}`}>edit</Link>
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 };
